35484d87ee1478e69a2ca149711df742e3c237a2
Angellyn218 <angellyncervantes@yahoo.com>
2022-11-03 15:59:53 -0700

    Added unique false to models to allow for m:n relationships

--- models/index.js
+++ models/index.js
@@ -17,10 +17,22 @@ Product.belongsTo(Category, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, { 
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, { 
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products'
+});
 
 module.exports = {
   Product,
