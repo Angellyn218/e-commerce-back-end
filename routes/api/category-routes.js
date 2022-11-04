034d5f9f7b135e8d4b88c692255208e0eb91e1d6
Angellyn218 <angellyncervantes@yahoo.com>
2022-11-03 18:38:12 -0700

    Added updated fuctionality to categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,8 +45,34 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const CategoryData = await Category.update({
+      category_name: req.body.category_name
+    }, {
+      where: {
+        id: req.params.id
+      }
+    })
+
+    const obj = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+    
+    if (!obj) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    } else if (CategoryData[0] === 0) {
+      res.status(404).json({ message: 'Category not updated!' });
+      return;
+    }
+
+    CategoryData.push(obj);
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
