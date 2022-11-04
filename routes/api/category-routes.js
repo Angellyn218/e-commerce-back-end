60e0d86b1abafaa92c55c6df74491eb8b11bfef9
Angellyn218 <angellyncervantes@yahoo.com>
2022-11-03 17:20:03 -0700

    Added post route to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,6 +37,12 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const CategoryData = await Category.create(req.body);
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
